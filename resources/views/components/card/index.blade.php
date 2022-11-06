4f6e476f3c3995c7aab786e6bf3b360ccaf8695d
andreslqr <me@andreslopez.com.mx>
2022-11-06 09:21:36 -0600

    added card component

---
+++ resources/views/components/card/index.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'content',
+    'topImage',
+    'bottomImage',
+    'actions',
+    'compact' => false,
+    'imageOnSide' => false,
+    'glass' => false,
+    'fullImage' => false
+])
+
+
+<div {{ $attributes->merge(['class' => 'card'])->class(['card-side' => $imageOnSide, 'glass' => $glass, 'card-compact' => $compact, 'image-full' => $fullImage]) }}>
+    @isset($topImage)
+        <figure {{ $topImage->attributes }}>
+            {{ $topImage }}    
+        </figure>
+    @endisset
+    <div class="card-body">
+        {{ $content ?? $slot }}
+        @isset($actions)
+            <div {{ $actions->atributes->merge(['class' => 'card-actions']) }}>
+                {{ $actions }}
+            </div>
+        @endisset
+    </div>
+    @isset($bottomImage)
+        <figure {{ $bottomImage->attributes }}>
+            {{ $bottomImage }}
+        </figure>
+    @endisset
+</div>
