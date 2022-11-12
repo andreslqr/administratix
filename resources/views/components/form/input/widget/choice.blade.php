4ba31917415d25ab0ff84d0fab0bc0e47ab1dc37
andreslqr <me@andreslopez.com.mx>
2022-11-12 00:12:06 +0000

    added styles

--- resources/views/components/form/input/widget/choice.blade.php
+++ resources/views/components/form/input/widget/choice.blade.php
@@ -15,7 +15,7 @@
 @endphp
 
 <div x-data="{selected: {{ $var }},  instance: null}" wire:ignore>
-    <select x-init="instance = new Choices($el, {
+    <select multiple x-init="instance = new Choices($el, {
         choices: [{
   value: 'Option 1',
   label: 'Option 1',
@@ -33,32 +33,9 @@
 }]
 ,
         classNames: {
-            containerOuter: 'choices',
-            containerInner: '{{ $attributes->merge(['class' => 'input input-bordered'])->get('class') }}',
-            input: 'choices__input',
-            inputCloned: 'choices__input--cloned',
-            list: 'menu',
-            listItems: 'choices__list--multiple',
-            listSingle: 'h-full justify-center',
-            listDropdown: 'choices__list--dropdown',
-            item: 'choices__item',
-            itemSelectable: 'choices__item--selectable',
-            itemDisabled: 'choices__item--disabled',
-            itemChoice: 'choices__item--choice',
-            placeholder: 'choices__placeholder',
-            group: 'choices__group',
-            groupHeading: 'choices__heading',
-            button: 'choices__button',
-            activeState: 'is-active',
-            focusState: 'is-focused',
-            openState: 'is-open',
-            disabledState: 'is-disabled',
-            highlightedState: 'is-highlighted',
-            selectedState: 'is-selected',
-            flippedState: 'is-flipped',
-            loadingState: 'is-loading',
-            noResults: 'has-no-results',
-            noChoices: 'has-no-choices'
+            containerInner: 'flex input input-bordered gap-x-1',
+            listItems: 'flex h-full items-center gap-x-1',
+            item: 'badge py-2'
         },
     })">
 
