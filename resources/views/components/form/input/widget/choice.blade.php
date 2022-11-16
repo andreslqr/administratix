fb9d1a766e9959d9fca816bf08efd3c8e8cbbcae
andreslqr <me@andreslopez.com.mx>
2022-11-16 22:37:53 +0000

    added choices.js functionality

--- resources/views/components/form/input/widget/choice.blade.php
+++ resources/views/components/form/input/widget/choice.blade.php
@@ -1,43 +1,69 @@
 @props([
-
+    'multiple' => false,
+    'options' => false,
+    'keyName',
+    'valueName',
+    'config' => [],
+    'withoutDataTransformation' => false 
 ])
 
 
-@php 
+@php
     $name = $attributes->wire('model')->value;
     $defer = $attributes->wire('model')->hasModifier('defer');
+    $source = $attributes->wire('source')->value;
 
-     $var = $value ?? '0';
 
-    if($name) 
+    $var = $value ?? '0';
+
+    if($options && !$withoutDataTransformation) {
+        $options = \Illuminate\Support\Collection::wrap($options)->mapWithKeys(fn($item, $key) => [['value' => isset($valueName) ? $item[$valueName] : $key, 'label' => isset($keyName) ? $item[$keyName] : $item]]);
+    }
+    
+    if ($name) {
         $var = new \Illuminate\Support\HtmlString("\$wire.entangle('{$name}')" . ($defer ? '.defer' : ''));
-
+    }
+    
 @endphp
 
-<div x-data="{selected: {{ $var }},  instance: null}" wire:ignore>
-    <select multiple x-init="instance = new Choices($el, {
-        choices: [{
-  value: 'Option 1',
-  label: 'Option 1',
-  selected: true,
-  disabled: false,
-},
-{
-  value: 'Option 2',
-  label: 'Option 2',
-  selected: false,
-  customProperties: {
-    description: 'Custom description about Option 2',
-    random: 'Another random custom property'
-  },
-}]
-,
-        classNames: {
-            containerInner: 'flex input input-bordered gap-x-1',
-            listItems: 'flex h-full items-center gap-x-1',
-            item: 'badge py-2'
+<div x-data="{ selected: {{ $var }}, instance: null, @if($source) source: async (value) => {
+    let items = await $wire.call('{{ $source }}', value);
+
+    if(Array.isArray(items)) {
+        items = items.map((item, index) => {
+            return {
+                label: @if(isset($keyName)) item['{{ $keyName }}'] @else item @endif,
+                value: @if(isset($valueName)) item['{{ $valueName }}'] @else index @endif,
+            };
+        });
+    }
+    else if(typeof items == 'object') {
+        items = Object.keys(items).map((key) => {
+            return {
+                label: items[key]@if(isset($keyName))['{{ $keyName }}']@endif,
+                value: @if(isset($valueName))items[key]['{{ $valueName }}']@else key @endif
+            };
+        });
+    }
+    
+    return items;
+
+} @endif }" x-init="$watch('selected', value => instance.removeActiveItems() && instance.setChoiceByValue(value))" wire:ignore>
+    <select @if($multiple) multiple @endif x-init="instance = new Choices($el, {
+        ...{
+            @if($options && !$source)
+                choices: @js($options),
+            @endif
         },
-    })">
+        ...@js($config)
+    });
+    instance.passedElement.element.addEventListener('change', (e) => selected = instance.getValue(true));
+    @if($source)
+        instance.passedElement.element.addEventListener('search', (e) => instance.setChoices(() => source(e.detail.value)));
+        instance.passedElement.element.addEventListener('change', (e) => instance.setChoices(() => source(e.detail.value)));
+        instance.passedElement.element.addEventListener('showDropdown', (e) => instance.setChoices(() => source(e.detail.value)));
+    @endif
+    ">
 
     </select>
 </div>
