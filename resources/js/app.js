72213d5966e6ad488d3c1574bfd8339eacb9ee23
andreslqr <me@andreslopez.com.mx>
2022-11-18 22:11:52 +0000

    init editor

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 import "./plugins/flatpickr";
+import "./plugins/filepond";
 import "./plugins/choices";
+import "./plugins/editor";
 import "./plugins/alpinejs";
 import "./plugins/statics";
---
+++ resources/js/plugins/editor.js
@@ -0,0 +1,8 @@
+import EditorJS from '@editorjs/editorjs'; 
+
+
+window.Editor = EditorJS;
+
+window.editorPlugins = {
+    header: require('@editorjs/header'),
+};
