b1ea68018682fbb23c254d3683a7702652cd3331
andreslqr <me@andreslopez.com.mx>
2023-02-02 02:21:34 +0000

    files separated

--- resources/vite.config.js
+++ resources/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/admin/sass/app.scss',
-                'resources/admin/js/app.js'
+                'resources/admin/js/app.js',
+                ...fs.readdirSync('resources/admin/js/plugins').map((file) => `resources/admin/js/plugins/${file}`)
             ],
             refresh: true
         })
