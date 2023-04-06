0274b251ba82293121be939d6064b6a6a9c14096
andreslqr <me@andreslopez.com.mx>
2023-04-06 17:49:55 -0600

    form elements added

--- resources/views/components/form/input/widget/swap.blade.php
+++ resources/views/components/form/input/widget/swap.blade.php
@@ -6,7 +6,7 @@
 
 <label {{ $attributes->merge(['class' => 'swap'])->class(['swap-rotate' => $rotate])->whereDoesntStartWith('wire:model') }}>
     <input type="checkbox" {{ $attributes->whereStartsWith('wire:model') }} />
-    <div class="swap-on" {{ $on->attributes->merge(['class' => 'swap-on']) }}>
+    <div {{ $on->attributes->merge(['class' => 'swap-on']) }}>
         {{ $on }}
     </div>
     <div {{ $on->attributes->merge(['class' => 'swap-off']) }}>
