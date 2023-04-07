cee75d6408453925654c8101492b8357a98136c1
andreslqr <me@andreslopez.com.mx>
2023-04-06 20:24:59 -0600

    partial docs for time plguin

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,7 +2,13 @@ import Alpine from 'alpinejs';
 import Prism from 'prismjs';
 import "prismjs/components/prism-markup-templating.js";
 import "prismjs/components/prism-php.js";
+import flatpickr from "flatpickr";
+
+
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+window.flatpickr = flatpickr;
+
